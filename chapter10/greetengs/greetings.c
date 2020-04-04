dd5b46136b40c590ce18eb5c8f69c4b9731dab5c
nafuka <42476527+nafuka11@users.noreply.github.com>
2020-04-04 13:31:10 +0900

    Add greetings

---
+++ chapter10/greetengs/greetings.c
@@ -0,0 +1,37 @@
+/**
+ * @file greetings.c
+ * @brief Ctrl+Cを押した時に文字列を表示するプログラム
+ * @author nafuka11
+ * @date 2020-04-03
+ */
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void diediedie(int sig)
+{
+	puts ("Goodbye cruel world....\n");
+	exit(1);
+}
+
+int catch_signal(int sig, void (*handler)(int))
+{
+	struct sigaction action;
+	action.sa_handler = handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
+	return sigaction(sig, &action, NULL);
+}
+
+int main(void)
+{
+	if (catch_signal(SIGINT, diediedie) == -1) {
+		fprintf(stderr, "Can't map the handler");
+		exit(2);
+	}
+	char name[30];
+	printf("Enter your name: ");
+	fgets(name, 30, stdin);
+	printf("Hello %s\n", name);
+	return 0;
+}
